84be449517db27cab374803cbf278ac55ec3c838
Your Name <you@example.com>
2026-05-15 19:09:54 +0530

    minor changes

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,26 @@
+
+   
+
+
+
+    let js_1 = "amazing";
+    if(js_1 == "amazing") alert("JS is fun");
+
+    let firstname = "Gerorge";
+
+    console.log(firstname);
+    console.log("error");
+
+    let feturm = "hinge";
+
+    console.log(feturm);
+    
+    let rf= true;
+
+    console.log(typeof(rf));
+
+    const now_age = 2033;
+    const age_john = now_age - 1989;
+    const age_sarah = now_age - 1980;
+
+    console.log(age_john,age_sarah);
