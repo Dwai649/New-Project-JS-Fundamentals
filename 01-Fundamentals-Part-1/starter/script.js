47a705e88c87ee55157fd06f63c92401457089ac
Your Name <you@example.com>
2026-05-16 09:52:36 +0530

    modifications part 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -24,3 +24,51 @@
     const age_sarah = now_age - 1980;
 
     console.log(age_john,age_sarah);
+    
+    let x = 15
+    z = x*4
+    x += 10
+    console.log(z)
+    console.log(x)
+
+
+    const namePerson = "Adam"
+    const age = 27
+    const profession = "martial arts"
+
+    const new_scope = `My Name is ${namePerson}`;
+    
+    console.log(new_scope);
+
+    console.log(`Hi 
+    Whats todays main issues accross the world
+    Lets see and understand`)
+
+    const warehouse_stocks = 18
+    const no_of_stores = 3
+
+    if(warehouse_stocks%no_of_stores == 0)
+    {
+        console.log("Supplies can be sent");
+        
+    }
+    else{
+        console.log("Need Replenishment of Orders")
+
+
+    }
+
+
+
+    console.log(Boolean(0))
+    console.log(Boolean(true))
+    console.log(Boolean("abcd"))
+
+    const favourite = prompt("Enter a number")
+    console.log(favourite)
+
+    if(favourite == 23)
+    {
+        console.log("success")
+        
+    }
